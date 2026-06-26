a85cffcfdaf8a3b482bd54e5b3d558678ce6c2a2
LaskarHans <hanifadityawardhana@gmail.com>
2026-06-26 17:36:29 +0700

    commit ke 2

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('landing-page');
+});
+
+Route::get('/register', function () {
     return view('welcome');
 });
